f74d7b99b751f4c02d6e3dc4bc8cbd9f3939c376
Dennis Fuhrmann <dennis.fuhrmann@sap.com>
2025-07-25 23:17:23 +0200

    update Spring Boot application to scan additional base packages

--- backend/src/main/java/org/mycontract/backend/BackendApplication.java
+++ backend/src/main/java/org/mycontract/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package org.mycontract.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.mycontract.backend", "com.docmgr"})
 public class BackendApplication {
 	
 	public static void main(String[] args) {
